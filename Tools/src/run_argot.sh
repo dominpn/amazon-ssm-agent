e6b1f3875e9b9e7e26dd9526a0f8cd144d3dc0c1
Victor Nicolet <victornl@amazon.com>
2024-12-09 14:02:30 -0500

    Adding new targets to ssm-parameter-logging analysis with sanitizers.
    cr: https://code.amazon.com/reviews/CR-164910525

--- Tools/src/run_argot.sh
+++ Tools/src/run_argot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
  ### Fixed argot version - upgrade here carefully and check that analyses are still supported
- REQUIRED_ARGOT_VERSION="v0.3.2-alpha"
+ REQUIRED_ARGOT_VERSION="v0.3.3-alpha"
 
 
  log() {
