7f97d3cd99aa205ba56c2f775b452a1e184b8f9c
Rocky Li <ziyli@amazon.com>
2025-04-03 22:07:34 +0000

    Fix IPV6 address for IMDS in documentation and deny rule
    cr: https://code.amazon.com/reviews/CR-187758889

--- agent/appconfig/constants.go
+++ agent/appconfig/constants.go
@@ -256,7 +256,7 @@ const (
 )
 
 // Default deny list IP addresses for remote host port forwarding: IMDS (ipv4, ipv6); VPC (ipv4, ipv6); Amazon Time Sync (ipv4, ipv6); Amazon Windows license activation (2x ipv4, ipv6)
-var DefaultDeniedPortForwardingRemoteIPs = []string{"169.254.169.254", "fd00:ec2::254", "169.254.169.253", "fd00:ec2::253", "169.254.169.123", "fd00:ec2::123", "169.254.169.250", "169.254.169.251", "fd00:ec2::240"}
+var DefaultDeniedPortForwardingRemoteIPs = []string{"169.254.169.254", "fd00:ec2::254", "169.254.169.253", "fd00:ec2::253", "169.254.169.123", "fd00:ec2::123", "169.254.169.250", "169.254.169.251", "fd00:ec2::250"}
 
 // Document versions that are supported by this Agent version.
 // Note that 1.1 and 2.1 are deprecated schemas and hence are not added here.
